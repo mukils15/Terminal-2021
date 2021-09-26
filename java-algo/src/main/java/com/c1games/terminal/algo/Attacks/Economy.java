2337b6b6f212cf71b6cb6184ccb76848eb57f9a9
krishyshah <62708713+krishyshah@users.noreply.github.com>
2021-09-26 10:03:59 -0400

    created new logic for economy attack

--- java-algo/src/main/java/com/c1games/terminal/algo/Attacks/Economy.java
+++ java-algo/src/main/java/com/c1games/terminal/algo/Attacks/Economy.java
@@ -1,5 +1,7 @@
 package com.c1games.terminal.algo.Attacks;
 
+import java.util.List;
+
 import com.c1games.terminal.algo.Config;
 import com.c1games.terminal.algo.Coords;
 import com.c1games.terminal.algo.PlayerId;
@@ -12,11 +14,20 @@ import com.c1games.terminal.algo.units.UnitType;
 public class Economy{
     
     //Save money on attack - don't spend all
-    public static void Economy(int budget, GameState curr, UnitType pieces,  Config config, Coords best) {
+    public static void Economy(int budget, GameState curr,  Coords best) {
          int totalCost = 0;
-         while (totalCost < budget) {
-             curr.attemptSpawn(best, pieces);
-             totalCost += config.unitInformation.get(pieces.ordinal()).cost()[1];
+         if (budget >= 3) {
+        	 curr.attemptSpawn(best, UnitType.Demolisher);
+             totalCost += 3; 
+         }
+         if (best.x != 25 || best.y != 11) {
+        	 best =  new Coords(25, 11);
+         } else {
+        	 best = new Coords(24, 10);
+         }
+         while (totalCost <= budget) {
+             curr.attemptSpawn(best, UnitType.Interceptor);
+             totalCost += 1;
          }
     }   
 }
