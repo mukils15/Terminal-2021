429601c678ae13309d43771d5406aaed61aa9a2e
krishyshah <62708713+krishyshah@users.noreply.github.com>
2021-09-26 10:25:54 -0400

    updated with new logic from Jay

--- java-algo/src/main/java/com/c1games/terminal/algo/Attacks/DemolishScoutStagger.java
+++ java-algo/src/main/java/com/c1games/terminal/algo/Attacks/DemolishScoutStagger.java
@@ -11,23 +11,44 @@ import com.c1games.terminal.algo.units.UnitType;
 
 public class DemolishScoutStagger {
     
-    public DemolishScoutStagger(GameState curr, Coords best) {
-        int totalCost = 0;
-        for (int i = 0; i < curr.data.p1Stats.bits * 0.75; i++) {
-            curr.attemptSpawn(best, UnitType.Demolisher);
+    public static void DemolishScoutStagger(GameState curr, Coords best) {
+        
+    	int bitsRemaining = (int) curr.data.p1Stats.bits;
+    	if (best.x < 13) {
+        	for (int i = 0; i < bitsRemaining * 0.75; i++) {
+                curr.attemptSpawn(new Coords(10, 3), UnitType.Demolisher);
+            }
+        	bitsRemaining = (int) curr.data.p1Stats.bits;
+        	for (int i = 0; i <= bitsRemaining; i ++) {
+        		curr.attemptSpawn(new Coords(11, 2), UnitType.Scout);
+        	}
+        	
+        }  else {
+        	for (int i = 0; i < curr.data.p1Stats.bits * 0.75; i++) {
+                curr.attemptSpawn(new Coords(14, 0), UnitType.Demolisher);
+            }
+        	bitsRemaining = (int) curr.data.p1Stats.bits;
+        	for (int i = 0; i <= bitsRemaining; i ++) {
+        		curr.attemptSpawn(new Coords(11, 2), UnitType.Scout);
+        	}
+        	
         }
         
-        while (curr.data.p1Stats.bits > 0) {
-            if (best.x < 13) {
-                Coords scout = new Coords(best.x + 1, best.y-1);
-                curr.attemptSpawn(scout, UnitType.Scout);
-            } else if (best.x > 13) {
-                Coords scout = new Coords(best.x + 1, best.y + 1);
-                curr.attemptSpawn(scout, UnitType.Scout);
-            } else {
-                Coords scout = new Coords(14, 0);
-                curr.attemptSpawn(scout, UnitType.Scout);
-            }
-        }
+//        for (int i = 0; i < curr.data.p1Stats.bits * 0.75; i++) {
+//            curr.attemptSpawn(best, UnitType.Demolisher);
+//        }
+//        
+//        while (curr.data.p1Stats.bits > 0) {
+//            if (best.x < 13) {
+//                Coords scout = new Coords(best.x + 1, best.y-1);
+//                curr.attemptSpawn(scout, UnitType.Scout);
+//            } else if (best.x > 13) {
+//                Coords scout = new Coords(best.x + 1, best.y + 1);
+//                curr.attemptSpawn(scout, UnitType.Scout);
+//            } else {
+//                Coords scout = new Coords(14, 0);
+//                curr.attemptSpawn(scout, UnitType.Scout);
+//            }
+//        }
     }
 }
